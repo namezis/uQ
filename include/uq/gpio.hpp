de2550ee44d633d8253bdeae472a698d9ce27da1
djowel <djowel@gmail.com>
2018-06-06 10:40:12 +0800

    Some cleanup

--- include/uq/gpio.hpp
+++ include/uq/gpio.hpp
@@ -12,6 +12,9 @@
 
 namespace cycfi { namespace uq
 {
+   ////////////////////////////////////////////////////////////////////////////
+   // Constants
+   ////////////////////////////////////////////////////////////////////////////
    enum class port_enum
    {
       a, b, c, d, e, f, g, h, i, j, k
@@ -19,29 +22,36 @@ namespace cycfi { namespace uq
 
    enum class port_speed_enum
    {
-      low_speed         = GPIO_SPEED_FREQ_LOW,        // 2MHz low speed
-      mid_speed         = GPIO_SPEED_FREQ_MEDIUM,     // 12.5 MHz to 50 MHz medium speed
-      high_speed        = GPIO_SPEED_FREQ_HIGH,       // 25 MHz to 100 MHz high speed
-      very_high_speed   = GPIO_SPEED_FREQ_VERY_HIGH   // 50 MHz to 200 MHz very high speed
+      // 2MHz low speed
+      low_speed = GPIO_SPEED_FREQ_LOW
+
+      // 12.5 MHz to 50 MHz medium speed
+      mid_speed  = GPIO_SPEED_FREQ_MEDIUM,
+
+      // 25 MHz to 100 MHz high speed
+      high_speed = GPIO_SPEED_FREQ_HIGH,
+
+      // 50 MHz to 200 MHz very high speed
+      very_high_speed = GPIO_SPEED_FREQ_VERY_HIGH
    };
 
    enum class port_mode_enum
    {
-      push_pull = GPIO_MODE_OUTPUT_PP                 // Output Push Pull Mode
-    , open_drain = GPIO_MODE_OUTPUT_OD                // Output Open Drain Mode
+      push_pull = GPIO_MODE_OUTPUT_PP   // Output Push Pull Mode
+    , open_drain = GPIO_MODE_OUTPUT_OD  // Output Open Drain Mode
    };
 
    enum class port_pull_enum
    {
-      none = GPIO_NOPULL                              // No Pull-up or Pull-down
-    , pull_up = GPIO_PULLUP                           // Pull-up
-    , pull_down = GPIO_PULLDOWN                       // Pull-down
+      none = GPIO_NOPULL                // No Pull-up or Pull-down
+    , pull_up = GPIO_PULLUP             // Pull-up
+    , pull_down = GPIO_PULLDOWN         // Pull-down
    };
 
    enum class port_edge_enum
    {
-      rising                                          // Rising edge
-    , falling                                         // Falling edge
+      rising                            // Rising edge
+    , falling                           // Falling edge
    };
 
    namespace port
@@ -74,6 +84,8 @@ namespace cycfi { namespace uq
       auto constexpr falling_edge         = port_edge_enum::falling;
    }
 
+   ////////////////////////////////////////////////////////////////////////////
+   // Support
    ////////////////////////////////////////////////////////////////////////////
    template <typename Derived>
    struct port_base
