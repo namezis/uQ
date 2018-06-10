6a7c39a11afaa66d0b5fb3ca4190b2b40598e07c
djowel <djowel@gmail.com>
2018-06-11 06:18:57 +0800

    proper project layout with tests

---
+++ tests/tests.cpp
@@ -0,0 +1,25 @@
+/*=============================================================================
+   Copyright (c) 2014-2018 Joel de Guzman. All rights reserved.
+
+   Distributed under the Boost Software License, Version 1.0. (See accompanying
+   file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+
+#define TIMER_TEST1
+
+#ifdef GPIO_TEST1
+# include "gpio1.cpp"
+#endif
+
+#ifdef GPIO_TEST2
+# include "gpio2.cpp"
+#endif
+
+#ifdef GPIO_TEST3
+# include "gpio3.cpp"
+#endif
+
+#ifdef TIMER_TEST1
+# include "timer1.cpp"
+#endif
+
--- tests/timer1.cpp
+++ tests/timer1.cpp
@@ -12,10 +12,13 @@ namespace uq = cycfi::uq;
 // Toggle led test using timers and interrupts. This test uses a timer to
 // toggle the dev-board's main led at a rate of 1 per second. No setup
 // required.
+//
+// The timer used has a clock frequency of 10kHz and a cycle period of 10000
+// ticks. Hence, 10kHz/10000 == 1 second.
 ///////////////////////////////////////////////////////////////////////////////
 
 uq::main_led   led;
-uq::timer<3>   timer(10000, 10000); // 10kHz freq, 10000 period = 1 second
+uq::timer<3>   timer(10000, 10000);
 
 int main(void)
 {
